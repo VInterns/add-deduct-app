b5a462279fe2a0338ff011aab3dcbb69ff2d9403
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-03-16 16:12:28 +0200

    added salary adjust screen for testing now

---
+++ client/src/screens/salary-adjust-screen.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export class SalaryAdjustmentScreen extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                SalaryAdjustmentScreen
+            </div>
+        );
+    }
+} 
