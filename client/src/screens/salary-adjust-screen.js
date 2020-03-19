48675e2edb5a7c58e895d92aac7e9510fa7f28e4
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-03-19 19:21:49 +0200

    added teams folder and updated salary adjustment screen

--- client/src/screens/salary-adjust-screen.js
+++ client/src/screens/salary-adjust-screen.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Team } from "../components";
-import { salaryAdjustHeader } from "../headers";
+import { SalaryAdjustment } from "../teams";
 
-let fileLink = "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e6f7b3e6701140739e1f29d/d0867743375c8fdcec5152090ecdba64/samples-sa.xlsx"
 export class SalaryAdjustmentScreen extends React.Component {
     render() {
         return (
             <Team
-                teamName='Salary Adjustment'
-                filePath={fileLink}
-                tableHeader={salaryAdjustHeader}
+                teamName={SalaryAdjustment.NAME}
+                filePath={SalaryAdjustment.SAMPLE_EXCEL_URL}
+                tableHeader={SalaryAdjustment.TABLE_HEADER}
             />
         );
     }
---
+++ client/src/teams/index.js
@@ -0,0 +1,9 @@
+const SalaryAdjustment = {
+    "NAME": "Salary Adjustment",
+    "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e6f7b3e6701140739e1f29d/d0867743375c8fdcec5152090ecdba64/samples-sa.xlsx",
+    "TABLE_HEADER": [ "Vendor", "Staff ID", "Roles", "Name", "Basic Net", "GB", "Net Salary", "New Basic Net Salary", "GB New", "Total", "Effective Date" ]
+}
+
+module.exports = {
+    SalaryAdjustment
+}
