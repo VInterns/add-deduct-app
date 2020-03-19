98754ca1e30936887a0bf38119867f115b4362ee
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-03-19 19:39:42 +0200

    converted salary adjustment screen to functional component

--- client/src/screens/salary-adjust-screen.js
+++ client/src/screens/salary-adjust-screen.js
@@ -2,14 +2,12 @@ import React from "react";
 import { Team } from "../components";
 import { SalaryAdjustment } from "../teams";
 
-export class SalaryAdjustmentScreen extends React.Component {
-    render() {
-        return (
-            <Team
-                teamName={SalaryAdjustment.NAME}
-                filePath={SalaryAdjustment.SAMPLE_EXCEL_URL}
-                tableHeader={SalaryAdjustment.TABLE_HEADER}
-            />
-        );
-    }
+export const SalaryAdjustmentScreen = () => {
+    return (
+        <Team
+            teamName={SalaryAdjustment.NAME}
+            filePath={SalaryAdjustment.SAMPLE_EXCEL_URL}
+            tableHeader={SalaryAdjustment.TABLE_HEADER}
+        />
+    );
 } 
