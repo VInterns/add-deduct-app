f1914e2e65646ed57765c380f4d8c237808341ee
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-03-24 18:46:04 +0200

    added api folder

---
+++ client/src/api/index.js
@@ -0,0 +1,8 @@
+const API_ROOT_PATH = "/api";
+const DATA_API_PATH = "/data";
+
+const SUBMIT_DATA_API = `${API_ROOT_PATH}${DATA_API_PATH}/submit_data`;
+
+module.exports = {
+    SUBMIT_DATA_API
+}
