62940ce02216297550bee0d46121a32132eca720
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-03-24 20:11:53 +0200

    added export data api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,9 @@ const API_ROOT_PATH = "/api";
 const DATA_API_PATH = "/data";
 
 const SUBMIT_DATA_API = `${API_ROOT_PATH}${DATA_API_PATH}/submit_data`;
+const EXPORT_DATA_API = `${API_ROOT_PATH}${DATA_API_PATH}/export_data`;
 
 module.exports = {
-    SUBMIT_DATA_API
+    SUBMIT_DATA_API,
+    EXPORT_DATA_API
 }
