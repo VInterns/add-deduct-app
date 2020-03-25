2de091be9967de89125654f33211cef2b4410260
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-03-25 22:31:44 +0200

    fix: correct excel file path

--- server/services/file.js
+++ server/services/file.js
@@ -1,7 +1,7 @@
 const XLSX = require("xlsx");
 
 const exportExcel = (fileName, jsonData) => {
-    let path = `${__dirname}/../public/sheets/${fileName}.xlsx`;
+    let path = `${__dirname}/../public/${fileName}.xlsx`;
     let workbook = XLSX.readFile(path);
     let worksheet = workbook.Sheets[workbook.SheetNames[0]];
     XLSX.utils.sheet_add_json(worksheet, jsonData, { skipHeader: true, origin: -1 });
