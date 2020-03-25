af7b333741dccc63a2e81611218c29c68e52cd34
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-03-25 22:28:50 +0200

    chore: update table header, ui classes & add button submit method

--- client/src/components/hr-team-table.js
+++ client/src/components/hr-team-table.js
@@ -1,30 +1,50 @@
 import React from "react";
+import download from "downloadjs";
 import PropTypes from "prop-types";
-import { Table, Icon, Button } from "semantic-ui-react";
-import { TeamTableHeader } from "../components";
+import {
+    Table,
+    Icon,
+    Button,
+    Header,
+    Label
+} from "semantic-ui-react";
+import { EXPORT_DATA_API } from "../api";
 
 export const HRTeamTable = (props) => {
     return (
         <div>
-            <Table>
+            <Table celled>
                 <Table.Header>
                     <Table.Row>
-                        <TeamTableHeader header={props.header} />
+                        <Table.HeaderCell colSpan={2}>
+                            <Header as="h3" className="p-3">{props.header}</Header>
+                        </Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
                 <Table.Body className="p-5">
-                    {props.body.map((row, index) => {
-                        let { fileName, lastUploadBy, lastUploadAt } = row;
+                    {props.body.map((file, index) => {
+
                         return (
                             <Table.Row key={index}>
                                 <Table.Cell collapsing verticalAlign='middle'>
-                                    <Icon name="file excel outline" color="green" size="large"/>
-                                    {fileName}
+                                    <Label className="bg-white" size="large">
+                                        <Icon name="file excel" color="green" size="large" />
+                                        {file}
+                                    </Label>
                                 </Table.Cell>
-                                <Table.Cell verticalAlign='middle'>{lastUploadBy}</Table.Cell>
-                                <Table.Cell verticalAlign='middle'>{lastUploadAt}</Table.Cell>
-                                <Table.Cell verticalAlign='middle'>
-                                    <Button icon basic size="tiny">
+                                <Table.Cell verticalAlign='middle' textAlign="center" collapsing>
+                                    <Button
+                                        icon
+                                        basic
+                                        size="tiny"
+                                        onClick={async (event) => {
+                                            event.preventDefault();
+                                            let API_URL = EXPORT_DATA_API + `?file_name=${file}`;
+                                            let res = await fetch(API_URL);
+                                            let blob = await res.blob();
+                                            download(blob, file);
+                                        }}
+                                    >
                                         <Icon name="download" />
                                     </Button>
                                 </Table.Cell>
@@ -38,6 +58,6 @@ export const HRTeamTable = (props) => {
 }
 
 HRTeamTable.propTypes = {
-    header: PropTypes.arrayOf(String).isRequired,
+    header: PropTypes.string.isRequired,
     body: PropTypes.array.isRequired
 }
