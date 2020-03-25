890c2e4161a4691b0a793be2ff82b62173844068
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-03-25 22:30:46 +0200

    chore: remove HEADER array and update it with a string

--- client/src/screens/hr-screen.js
+++ client/src/screens/hr-screen.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { HRTeam } from "../components";
 
-const HEADER = [
-    "Final Excel File",
-    "Last Upload By",
-    "Last Upload At",
-    ""
-]
-
 export const HRScreen = () => {
     return (
         <HRTeam
             teamName={"Human Resources"}
-            tableHeader={HEADER}
+            tableHeader={"Grouped Excel Files"}
         />
     )
 }
