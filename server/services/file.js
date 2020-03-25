982d1dd923567278fdbe7e87b0cb588552fd6740
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-03-25 18:57:40 +0200

    fix: remove extra line & change parameter name to fileName

--- server/services/file.js
+++ server/services/file.js
@@ -1,8 +1,7 @@
 const XLSX = require("xlsx");
 
-
-const exportExcel = (collection, jsonData) => {
-    let path = `${__dirname}/../public/sheets/${collection}.xlsx`;
+const exportExcel = (fileName, jsonData) => {
+    let path = `${__dirname}/../public/sheets/${fileName}.xlsx`;
     let workbook = XLSX.readFile(path);
     let worksheet = workbook.Sheets[workbook.SheetNames[0]];
     XLSX.utils.sheet_add_json(worksheet, jsonData, { skipHeader: true, origin: -1 });
