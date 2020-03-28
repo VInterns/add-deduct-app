777df4d977f5f165056f89d7fe86b238203887e6
Mohamed Adel <mo.adel@nu.edu.eg>
2020-03-29 00:10:36 +0200

    Adjust HR table Componet to be show on Tabs for each file

--- client/src/components/hr-team-table.js
+++ client/src/components/hr-team-table.js
@@ -6,13 +6,37 @@ import {
     Icon,
     Button,
     Header,
-    Label
+    Label,
+    Tab
 } from "semantic-ui-react";
 import { EXPORT_DATA_API } from "../api";
 
+const panes = [
+    { menuItem: 'Salary Adjustment', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'Over Time', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'Net Expenses', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'Night Shift Amount Days', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'Lost Hours', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'Overview Reward Gross ', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'No Show Detailed', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'No Shows', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'Incentives Gross ', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'Incentives Net', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'Mobile Bill', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'Sick Leave Detailed', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'Sick Leave Cons', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'UK-DE Referral Prog Net Amount', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'Award Gross Amount', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'GYM', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'Penalties Detailed', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'On Call', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+     { menuItem: 'Profile Change', render: () => <Tab.Pane><Button icon basic  > <Icon name="download" /> Download</Button></Tab.Pane> },
+    
+]
+
 export const HRTeamTable = (props) => {
     return (
-        <div>
+ /*        <div>
             <Table celled>
                 <Table.Header>
                     <Table.Row>
@@ -22,6 +46,7 @@ export const HRTeamTable = (props) => {
                     </Table.Row>
                 </Table.Header>
                 <Table.Body className="p-5">
+   
                     {props.body.map((file, index) => {
 
                         return (
@@ -52,8 +77,11 @@ export const HRTeamTable = (props) => {
                         )
                     })}
                 </Table.Body>
-            </Table>
-        </div>
+            </Table> */
+            <div>
+            <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={panes} />
+            
+            </div>
     )
 }
 
