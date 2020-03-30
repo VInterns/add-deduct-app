b10b16f858282332f705bd19c1c0eba64ac5708c
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-03-30 17:11:39 +0200

    refactor: delete convertFromCamelCase as it's unused

--- client/src/util/index.js
+++
@@ -1,9 +0,0 @@
-const convertFromCamelCase = (string) => {
-    return string
-        .replace(/([A-Z])/g, ' $1')
-        .replace(/^./, function (str) { return str.toUpperCase(); });
-}
-
-module.exports = {
-    convertFromCamelCase
-}
