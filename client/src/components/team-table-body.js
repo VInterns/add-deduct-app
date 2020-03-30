4b5ab5a682e8c1b7f5515e98edbd1b0d57745c61
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-03-30 22:58:53 +0200

    refactor: update rows array

--- client/src/components/team-table-body.js
+++ client/src/components/team-table-body.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Table } from "semantic-ui-react";
 import { TeamTableBodyRow } from "../components";
 
-
 export const TeamTableBody = (props) => {
-    let renderRows = props.rows.slice(1);
-    return renderRows.map((row, index) => {
+    let { rows } = props;
+    return rows.map((row, index) => {
         return (
             <Table.Row key={index}>
-                <TeamTableBodyRow key={index} data={row} keys={props.rows[0]}/>
+                <TeamTableBodyRow key={index} data={row} keys={props.rows[0]} />
             </Table.Row>
         )
     })
