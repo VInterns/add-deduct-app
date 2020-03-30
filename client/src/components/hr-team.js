0c9ec700dc53a0fd2c60f68bb7796917f8c52b2f
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-03-30 17:18:48 +0200

    refactor: remove file names and HRTeamTable body

--- client/src/components/hr-team.js
+++ client/src/components/hr-team.js
@@ -1,38 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Container } from "semantic-ui-react";
-import { TeamHeader, HRTeamBody, HRTeamTable } from "../components";
+import { TeamHeader, HRTeamTable } from "../components";
 
-// Collections
-const FILES = [
-  "salary-adjustment.xlsx",
-  "overtime.xlsx",
-  "net-expenses.xlsx",
-  "night-shift-amount-days.xlsx",
-  "lost-hours.xlsx",
-  "overtime-reward-gross.xlsx",
-  "no-show-detailed.xlsx",
-  "no-shows.xlsx",
-  "incentives-gross.xlsx",
-  "incentives-net.xlsx",
-  "mobile-bill.xlsx",
-  "sick-leave-detailed.xlsx",
-  "sick-leave-cons.xlsx",
-  "ukde-referral-prog-net-amount.xlsx",
-  "award-gross-amount.xlsx",
-  "gym.xlsx",
-  "penalties-detailed.xlsx",
-  "on-call.xlsx",
-  "profile-change.xlsx"
-];
-
-export const HRTeam = props => {
+export const HRTeam = (props) => {
   return (
     <Container fluid className="bg-light p-5" style={{ height: "160vh" }}>
       <div className="offset-md-1 col-md-10 border bg-white rounded p-5">
         <TeamHeader team={props.teamName} />
-        <HRTeamBody />
-        <HRTeamTable body={FILES} header={props.tableHeader} />
+        <HRTeamTable header={props.tableHeader} />
       </div>
     </Container>
   );
