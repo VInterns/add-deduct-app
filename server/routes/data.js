f4d72e431cb63604ab11c1aed6ca953a1214447f
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-03-31 19:49:34 +0200

    refactor: add session user to normalized data

--- server/routes/data.js
+++ server/routes/data.js
@@ -8,13 +8,14 @@ module.exports = db => {
 
   // API: IMPORT DATA
   router.post("/submit_data", (req, res) => {
+    let user = req.user;
     let data = req.body.what_to_submit;
     let collection = req.body.where_to_submit;
 
     // data received desc: array of arrays => we need to normalize it to array of objects
     // [[val], [val], [val]] => [{0:val}, {1:val}, {2:val}]
     // replace number keys with the actual keys
-    let normalized = normalizeData(data[0], data.slice(1));
+    let normalized = normalizeData(data[0], data.slice(1), user);
 
     db.collection(collection).insertMany(normalized, function (err, result) {
       if (err) {
