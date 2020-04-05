cc4541506036c46ded4af8fcb9ff7049e77cca99
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-04-05 21:49:41 +0200

    feat: add each team initial route

--- client/src/teams/index.js
+++ client/src/teams/index.js
@@ -1,22 +1,25 @@
 const SAT = {
     "NAME": "Salary Adjustment",
     "COLLECTION_NAME": "salary-adjustment",
+    "INITIAL_ROUTE": "/salary_adjust",
     "TABLE_HEADER": ["Vendor", "Staff ID", "Roles", "Name", "Basic Net", "GB", "Net Salary", "New Basic Net Salary", "GB New", "Total", "Effective Date"],
     "PANE_KEYS": ["Vendor", "Staff ID", "Roles", "Name", "Basic Net", "GB", "Net Salary", "New Basic Net Salary", "GB New", "Total", "Effective Date", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/ce92ff81cc4b7a041251510e3569063b/salary-adjustment-sample.xlsx",
 }
 
-const OT = {
+const OVT = {
     "NAME": "Overtime",
     "COLLECTION_NAME": "overtime",
+    "INITIAL_ROUTE": "/overtime",
     "TABLE_HEADER": ["Vendor", "ID", "Name", "Overtime Hours", "Logged Out"],
     "PANE_KEYS": ["Vendor", "ID", "Name", "Overtime Hours", "Logged Out", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/85ac0ebe053428770a4d1e4feca5dd50/overtime-sample.xlsx",
 }
 
-const NET = {
+const NEXT = {
     "NAME": "Net Expenses",
     "COLLECTION_NAME": "net-expenses",
+    "INITIAL_ROUTE": "/net_expenses",
     "TABLE_HEADER": ["Vendor", "ID", "Amount"],
     "PANE_KEYS": ["Vendor", "ID", "Amount", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/1d814ff23a3bc8281c0268704f1b422f/net-expenses-sample.xlsx"
@@ -25,6 +28,7 @@ const NET = {
 const NSADT = {
     "NAME": "Night Shift Amount Days",
     "COLLECTION_NAME": "night-shift-amount-days",
+    "INITIAL_ROUTE": '/night_shift_amount_days',
     "TABLE_HEADER": ["Vendor", "ID", "Night Shift Amount", "Night Shift Days"],
     "PANE_KEYS": ["Vendor", "ID", "Night Shift Amount", "Night Shift Days", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/94a03f78d8e086768ba1b2599bfa75c0/night-shift-amount-days-sample.xlsx"
@@ -33,6 +37,7 @@ const NSADT = {
 const LHT = {
     "NAME": "Lost Hours",
     "COLLECTION_NAME": "lost-hours",
+    "INITIAL_ROUTE": "/lost_hours",
     "TABLE_HEADER": ["Vendor", "ID", "Lost Hours Deduction", "Lost Hours Refund"],
     "PANE_KEYS": ["Vendor", "ID", "Lost Hours Deduction", "Lost Hours Refund", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/3f139dabfbf7e336cd96854b3e9a886c/lost-hours-sample.xlsx"
@@ -41,22 +46,25 @@ const LHT = {
 const ORGT = {
     "NAME": "Overtime Reward Gross",
     "COLLECTION_NAME": "overtime-reward-gross",
+    "INITIAL_ROUTE": "/overtime_reward_gross",
     "TABLE_HEADER": ["Vendor", "Staff ID", "Name", "Total Reward Payment"],
     "PANE_KEYS": ["Vendor", "Staff ID", "Name", "Total Reward Payment", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/377b20bc67db15c40ab2a40c1141f084/overtime-reward-gross-sample.xlsx"
 }
 
-const NSDT = {
+const NSHDT = {
     "NAME": "No Show Detailed",
     "COLLECTION_NAME": "no-show-detailed",
+    "INITIAL_ROUTE": "/no_show_detailed",
     "TABLE_HEADER": ["IEX", "Date", "Exception", "Name", "MU", "MU Name", "Manager", "Supervisor", "Staff Number", "Account", "Vendor"],
     "PANE_KEYS": ["IEX", "Date", "Exception", "Name", "MU", "MU Name", "Manager", "Supervisor", "Staff Number", "Account", "Vendor", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/53fbb6f30b85f76fcaa252a6c6ef81f6/no-show-detailed-sample.xlsx"
 }
 
-const NST = {
+const NSHT = {
     "NAME": "No Shows",
     "COLLECTION_NAME": "no-shows",
+    "INITIAL_ROUTE": "/no_shows",
     "TABLE_HEADER": ["Vendor", "ID", "Name", "Total Deducted Days", "Refunded Days"],
     "PANE_KEYS": ["Vendor", "ID", "Name", "Total Deducted Days", "Refunded Days", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/13ed7848e1118b191f16c1b0ca62c31f/no-shows-sample.xlsx"
@@ -65,6 +73,7 @@ const NST = {
 const IGT = {
     "NAME": "Incentives Gross",
     "COLLECTION_NAME": "incentives-gross",
+    "INITIAL_ROUTE": "/incentives_gross",
     "TABLE_HEADER": ["Vendor", "Staff ID", "Name", "Total Gross Incentives", "Account"],
     "PANE_KEYS": ["Vendor", "Staff ID", "Name", "Total Gross Incentives", "Account", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/8e3c913868ae9bb82a12a98c397cacf6/incentives-gross-sample.xlsx"
@@ -73,6 +82,7 @@ const IGT = {
 const INT = {
     "NAME": "Incentives Net",
     "COLLECTION_NAME": "incentives-net",
+    "INITIAL_ROUTE": "/incentives_net",
     "TABLE_HEADER": ["Vendor", "Staff ID", "Name", "Amount"],
     "PANE_KEYS": ["Vendor", "Staff ID", "Name", "Amount", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/c9277b6545431c33c4812057c9143c08/incentives-net-sample.xlsx"
@@ -81,6 +91,7 @@ const INT = {
 const MBT = {
     "NAME": "Mobile Bill",
     "COLLECTION_NAME": "mobile-bill",
+    "INITIAL_ROUTE": "/mobile_bill",
     "TABLE_HEADER": ["Staff ID", "Name", "Total Open Amount", "Vendor"],
     "PANE_KEYS": ["Staff ID", "Name", "Total Open Amount", "Vendor", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/0f4bdd70fb39cc40d2200e27785f5695/mobile-bill-sample.xlsx"
@@ -89,6 +100,7 @@ const MBT = {
 const SLDT = {
     "NAME": "Sick Leave Detailed",
     "COLLECTION_NAME": "sick-leave-detailed",
+    "INITIAL_ROUTE": "/sick_leave_detailed",
     "TABLE_HEADER": ["IEX", "Date", "Exception", "Name", "MU", "MU Name", "Manager", "Supervisor", "Staff Number", "Account", "Vendor"],
     "PANE_KEYS": ["IEX", "Date", "Exception", "Name", "MU", "MU Name", "Manager", "Supervisor", "Staff Number", "Account", "Vendor", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/fded0caf1f0c6088e5663fd369aab936/sick-leave-detailed-sample.xlsx"
@@ -97,6 +109,7 @@ const SLDT = {
 const SLCT = {
     "NAME": "Sick Leave Cons",
     "COLLECTION_NAME": "sick-leave-cons",
+    "INITIAL_ROUTE": "/sick_leave_cons",
     "TABLE_HEADER": ["Vendor", "ID", "Name", "Sum of Sick to be Deducted"],
     "PANE_KEYS": ["Vendor", "ID", "Name", "Sum of Sick to be Deducted", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/a5eb7a97362f869730d61425ca62cf2f/sick-leave-cons-sample.xlsx"
@@ -105,6 +118,7 @@ const SLCT = {
 const UKDET = {
     "NAME": "UK-DE Referral Prog Net Amount",
     "COLLECTION_NAME": "ukde-referral-prog-net-amount",
+    "INITIAL_ROUTE": "/ukde_referral",
     "TABLE_HEADER": ["Vendor", "ID", "Name", "Net"],
     "PANE_KEYS": ["Vendor", "ID", "Name", "Net", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/59d9b0b92268ec3379b0eaf62e820fe8/ukde-referral-prog-net-amount-sample.xlsx"
@@ -113,6 +127,7 @@ const UKDET = {
 const AGAT = {
     "NAME": "Award Gross Amount",
     "COLLECTION_NAME": "award-gross-amount",
+    "INITIAL_ROUTE": "/award_gross_amount",
     "TABLE_HEADER": ["Vendor", "Staff ID", "Names", "Amount of Reward"],
     "PANE_KEYS": ["Vendor", "Staff ID", "Names", "Amount of Reward", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/6f84eab7f0127677d7a57f65e4b69ee1/award-gross-amount-sample.xlsx"
@@ -121,6 +136,7 @@ const AGAT = {
 const GYMT = {
     "NAME": "GYM",
     "COLLECTION_NAME": "gym",
+    "INITIAL_ROUTE": "/gym",
     "TABLE_HEADER": ["Vendor", "ID", "Name", "Amount"],
     "PANE_KEYS": ["Vendor", "ID", "Name", "Amount", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/442ce6c8851cecba94b5b2d1c3a2d515/gym-sample.xlsx"
@@ -129,6 +145,7 @@ const GYMT = {
 const PDT = {
     "NAME": "Penalties Detailed",
     "COLLECTION_NAME": "penalties-detailed",
+    "INITIAL_ROUTE": "/penalties_detailed",
     "TABLE_HEADER": ["Vendor", "ID", "Violater Name", "Violation Type", "Incident Details", "Incident Date", "Penalty Applied", "Penalty Receiving Date"],
     "PANE_KEYS": ["Vendor", "ID", "Violater Name", "Violation Type", "Incident Details", "Incident Date", "Penalty Applied", "Penalty Receiving Date", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/f894c230d86d65a4c6bf5e06189c7eea/penalties-detailed-sample.xlsx"
@@ -137,6 +154,7 @@ const PDT = {
 const OCT = {
     "NAME": "On Call",
     "COLLECTION_NAME": "on-call",
+    "INITIAL_ROUTE": "/on_call",
     "TABLE_HEADER": ["Vendor", "ID", "Name", "Days"],
     "PANE_KEYS": ["Vendor", "ID", "Name", "Days", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/678ccb547ee460a46cfb1cdaf92b7436/on-call-sample.xlsx"
@@ -145,20 +163,26 @@ const OCT = {
 const PCT = {
     "NAME": "Profile Change",
     "COLLECTION_NAME": "profile-change",
+    "INITIAL_ROUTE": "/profile_change",
     "TABLE_HEADER": ["ID", "Vendor", "Name", "Effective Date", "Account", "New Basic", "New GB", "New Total"],
     "PANE_KEYS": ["ID", "Vendor", "Name", "Effective Date", "Account", "New Basic", "New GB", "New Total", "Submitted By", "Submitted At"],
     "SAMPLE_EXCEL_URL": "https://trello-attachments.s3.amazonaws.com/5e1f261085da7e5ef966e7ce/5e8396d671713937059e50e9/cb871660388e3bbea992c6b22de4339b/profile-change-sample.xlsx"
 }
 
+const HRT = {
+    "NAME": "Human Resources",
+    "INITIAL_ROUTE": "/hr"
+}
+
 module.exports = {
     SAT,            // Salary Adjustment Team
-    OT,             // Overtime Team
-    NET,            // Net Expenses Team
+    OVT,             // Overtime Team
+    NEXT,            // Net Expenses Team
     NSADT,          // Night Shift Amount Days Team
     LHT,            // Lost Hours Team
     ORGT,           // Overtime Reward Gross Team
-    NSDT,           // No Show Detailed Team
-    NST,            // No Shows Team
+    NSHDT,           // No Show Detailed Team
+    NSHT,            // No Shows Team
     IGT,            // Incentives Gross Team
     INT,            // Incentives Net Team
     MBT,            // Mobile Bill Team
@@ -169,5 +193,6 @@ module.exports = {
     GYMT,           // Gym Team 
     PDT,            // Penalties Detailed Team
     OCT,            // On Call Team
-    PCT             // Profile Change Team
+    PCT,            // Profile Change Team
+    HRT             // Human Resources Team
 }
