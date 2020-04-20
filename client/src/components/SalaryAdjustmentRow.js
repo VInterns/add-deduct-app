4875913246f76b5ac61f2e660b8549d7760c8b97
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-04-20 21:31:09 +0200

    feat: salary adjust excel workbook download component

---
+++ client/src/components/SalaryAdjustmentRow.js
@@ -0,0 +1,74 @@
+import React from "react";
+import {
+    Icon,
+    Image,
+    Dimmer,
+    Loader,
+    Segment,
+} from "semantic-ui-react";
+import _ from "lodash";
+import { ButtonWB } from ".";
+import { EXPORT_DATA_API } from "../api";
+import Workbook from "react-excel-workbook";
+
+export class SalaryAdjustmentRow extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: null,
+        }
+    }
+
+    componentDidMount() {
+        let API = EXPORT_DATA_API + '/' + 'salary-adjustment';
+        fetch(API, {
+            headers: { "Content-Type": "application/json" },
+            method: 'GET'
+        }).then(res => {
+            return res.json();
+        }).then(excelData => {
+            this.setState({
+                data: excelData
+            })
+        }).catch(err => {
+            throw err;
+        })
+    }
+
+    render() {
+        let fileName  = "salary-adjustment";
+        let { data } = this.state;
+
+        if (data !== null) {
+            console.log("not null", data)
+            return (
+                <div className="d-flex flex-row bg-light border rounded justify-content-between align-items-center mt-2 p-3">
+                    <div className="d-flex flex-row">
+                        <Icon name="file excel" color="green" size="large" />
+                        <p className="lead ml-2">{fileName + '.xlsx'}</p>
+                    </div>
+                    <div className="align-self-end">
+                        <Workbook filename={fileName + '.xlsx'} element={<ButtonWB />}>
+                            <Workbook.Sheet data={data} name={_.startCase(fileName)}>
+                                {Object.keys(data[0]).map((k, i) => {
+                                    return <Workbook.Column key={i} label={_.startCase(k)} value={k} />
+                                })}
+                            </Workbook.Sheet>
+                        </Workbook>
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <Segment>
+                    <Dimmer active inverted>
+                        <Loader inverted>Fetching Data</Loader>
+                    </Dimmer>
+                    <Image src='/images/wireframe/short-paragraph.png' />
+                </Segment>
+            )
+        }
+    }
+}
