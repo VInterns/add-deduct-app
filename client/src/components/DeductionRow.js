d2e7b949d5319cd47aab0ef46367e9062445311d
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-04-20 21:30:40 +0200

    feat: deduction excel workbook download component

---
+++ client/src/components/DeductionRow.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Icon } from "semantic-ui-react";
+import { ButtonWB } from "../components";
+import Workbook from "react-excel-workbook";
+
+export class DeductionRow extends React.Component {
+    render() {
+        let fileName = "deduction";
+        return (
+            <div className="d-flex flex-row bg-light border rounded justify-content-between align-items-center mt-2 p-3">
+                <div className="d-flex flex-row">
+                    <Icon name="file excel" color="green" size="large" />
+                    <p className="lead ml-2">{fileName + '.xlsx'}</p>
+                </div>
+                <div className="align-self-end">
+                    <Workbook filename={fileName + '.xlsx'} element={<ButtonWB />}>
+                    </Workbook>
+                </div>
+            </div>
+        )
+    }
+}
