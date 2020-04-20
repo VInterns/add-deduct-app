64bf08b958e2394c8ac5f5ebd7fdecccb5b09222
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-04-20 21:31:36 +0200

    feat: workbook custom download button

---
+++ client/src/components/ButtonWB.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button, Icon } from "semantic-ui-react";
+
+export const ButtonWB = () => {
+    return (
+        <Button icon basic>
+            <Icon name="download" />
+        </Button>
+    )
+}
