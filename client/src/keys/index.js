88049e898820ee71e905435e79967a557426bd2c
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-04-21 15:20:34 +0200

    added deduction excel sheet keys

--- client/src/keys/index.js
+++ client/src/keys/index.js
@@ -11,59 +11,51 @@ import {
     NSDT,
     ORGT,
     UKDET,
-    AGAT
+    AGAT,
+    LHT,
+    NSHDT,
+    NSHT,
+    MBT,
+    GYMT,
+    SLDT,
+    SLCT,
+    PDT
 } from '../teams';
 import _ from "lodash";
 
-export const IG_keys = IGT.PANE_KEYS.map(key => {
-    return _.camelCase(key)
-});
+const toCamelCase = (keysArray) => {
+    return keysArray.map(key => {
+        return _.camelCase(key)
+    })
+}
 
-export const IN_keys = INT.PANE_KEYS.map(key => {
-    return _.camelCase(key)
-});
+/* Addition */
+export const IG_keys = toCamelCase(IGT.PANE_KEYS);
+export const IN_keys = toCamelCase(INT.PANE_KEYS);
+export const OC_keys = toCamelCase(OCT.PANE_KEYS);
+export const OV_keys = toCamelCase(OVT.PANE_KEYS);
+export const AGA_keys = toCamelCase(AGAT.PANE_KEYS);
+export const NEX_keys = toCamelCase(NEXT.PANE_KEYS);
+export const NSA_keys = toCamelCase(NSAT.PANE_KEYS);
+export const NSD_keys = toCamelCase(NSDT.PANE_KEYS);
+export const ORG_keys = toCamelCase(ORGT.PANE_KEYS);
+export const UKDE_keys = toCamelCase(UKDET.PANE_KEYS);
 
-export const NH_keys = NHT.PANE_KEYS.map(key => {
-    return _.camelCase(key)
-})
+/* Deduction */
+export const LH_keys = toCamelCase(LHT.PANE_KEYS);
+export const MB_keys = toCamelCase(MBT.PANE_KEYS);
+export const PD_keys = toCamelCase(PDT.PANE_KEYS);
+export const GYM_keys = toCamelCase(GYMT.PANE_KEYS);
+export const NSH_keys = toCamelCase(NSHT.PANE_KEYS);
+export const SLC_keys = toCamelCase(SLCT.PANE_KEYS);
+export const SLD_keys = toCamelCase(SLDT.PANE_KEYS);
+export const NSHD_keys = toCamelCase(NSHDT.PANE_KEYS);
 
-export const OC_keys = OCT.PANE_KEYS.map(key => {
-    return _.camelCase(key)
-});
+/* Profile Change */
+export const PC_keys = toCamelCase(PCT.PANE_KEYS);
 
-export const OV_keys = OVT.PANE_KEYS.map(key => {
-    return _.camelCase(key)
-});
+/* Salary Adjustment */
+export const SA_keys = toCamelCase(SAT.PANE_KEYS);
 
-export const PC_keys = PCT.PANE_KEYS.map(key => {
-    return _.camelCase(key)
-})
-
-export const SA_keys = SAT.PANE_KEYS.map(key => {
-    return _.camelCase(key)
-})
-
-export const AGA_keys = AGAT.PANE_KEYS.map(key => {
-    return _.camelCase(key)
-});
-
-export const NEX_keys = NEXT.PANE_KEYS.map(key => {
-    return _.camelCase(key)
-});
-
-export const NSA_keys = NSAT.PANE_KEYS.map(key => {
-    return _.camelCase(key)
-});
-
-export const NSD_keys = NSDT.PANE_KEYS.map(key => {
-    return _.camelCase(key)
-});
-
-export const ORG_keys = ORGT.PANE_KEYS.map(key => {
-    return _.camelCase(key)
-});
-
-export const UKDE_keys = UKDET.PANE_KEYS.map(key => {
-    return _.camelCase(key)
-});
-
+/* New Hires */
+export const NH_keys = toCamelCase(NHT.PANE_KEYS);
