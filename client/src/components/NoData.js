985b8108a720dbc26f57b3d5acd2a64d5f982fdd
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-04-27 18:24:51 +0200

    update the header 2 text

--- client/src/components/NoData.js
+++ client/src/components/NoData.js
@@ -7,7 +7,7 @@ export const NoData = () => {
             <div className='bg-danger rounded p-5 d-flex flex-column align-items-center justify-content-center'>
                 <Icon name='frown' size='massive' inverted />
                 <h1 className='display-3 text-white'>Ooops...</h1>
-                <h2 className='text-white'>No data has been found for this month</h2>
+                <h2 className='text-white'>No data has been found for this month of year</h2>
             </div>
         </div>
     )
