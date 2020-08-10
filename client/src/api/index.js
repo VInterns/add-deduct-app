6a8d07eb9895e50db8bf0be0baada0c6f49c7125
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-08-10 19:51:21 +0200

    Export List All Users API Endpoint

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,10 +1,13 @@
 const API_ROOT_PATH = "/api";
 const DATA_API_PATH = "/data";
+const USERS_API_PATH = "/users";
 
 const SUBMIT_DATA_API = `${API_ROOT_PATH}${DATA_API_PATH}/submit_data`;
 const EXPORT_DATA_API = `${API_ROOT_PATH}${DATA_API_PATH}/export_data`;
+const LIST_USERS_API = `${API_ROOT_PATH}${USERS_API_PATH}/listUsers`;
 
 module.exports = {
-    SUBMIT_DATA_API,
-    EXPORT_DATA_API
-}
+  SUBMIT_DATA_API,
+  EXPORT_DATA_API,
+  LIST_USERS_API,
+};
