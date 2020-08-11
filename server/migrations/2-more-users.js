4632a2567f388c5592794ae7e36cdf34f145ec79
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-08-11 19:46:54 +0200

    Add User Status

--- server/migrations/2-more-users.js
+++ server/migrations/2-more-users.js
@@ -1,209 +1,192 @@
-'use strict';
+"use strict";
 
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-let collection = 'users';
+let collection = "users";
 
-module.exports.id = 'more-users';
+module.exports.id = "more-users";
 
 module.exports.up = function (done) {
+  let salt = bcrypt.genSaltSync();
 
-    let salt = bcrypt.genSaltSync();
+  // HR Admin
+  this.db.collection(collection).insertOne({
+    username: "hr_admin_2",
+    status: "activated",
+    password: bcrypt.hashSync("hradmin2", salt),
+    roles: ["ADA_HR"],
+  });
 
-    // HR Admin
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'hr_admin_2',
-            password: bcrypt.hashSync('hradmin2', salt),
-            roles: ['ADA_HR']
-        })
+  // Salary Adjust
+  this.db.collection(collection).insertOne({
+    username: "salary_adjust_2",
+    status: "activated",
+    password: bcrypt.hashSync("salary2", salt),
+    roles: ["ADA_SA"],
+  });
 
-    // Salary Adjust
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'salary_adjust_2',
-            password: bcrypt.hashSync('salary2', salt),
-            roles: ['ADA_SA']
-        })
+  // Overtime
+  this.db.collection(collection).insertOne({
+    username: "overtime_2",
+    status: "activated",
+    password: bcrypt.hashSync("overtime2", salt),
+    roles: ["ADA_OVT"],
+  });
 
-    // Overtime
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'overtime_2',
-            password: bcrypt.hashSync('overtime2', salt),
-            roles: ['ADA_OVT']
-        })
+  // Next Expense
+  this.db.collection(collection).insertOne({
+    username: "net_expenses_2",
+    status: "activated",
+    password: bcrypt.hashSync("ntxpns", salt),
+    roles: ["ADA_NEX"],
+  });
 
-    // Next Expense
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'net_expenses_2',
-            password: bcrypt.hashSync('ntxpns', salt),
-            roles: ['ADA_NEX']
-        })
+  // Night Shift
+  this.db.collection(collection).insertOne({
+    username: "night_shift_2",
+    status: "activated",
+    password: bcrypt.hashSync("nshft2", salt),
+    roles: ["ADA_NSAD"],
+  });
 
-    // Night Shift
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'night_shift_2',
-            password: bcrypt.hashSync('nshft2', salt),
-            roles: ['ADA_NSAD']
-        })
+  // Lost Hours
+  this.db.collection(collection).insertOne({
+    username: "lost_hours_2",
+    status: "activated",
+    password: bcrypt.hashSync("lohrs2", salt),
+    roles: ["ADA_LH"],
+  });
 
-    // Lost Hours
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'lost_hours_2',
-            password: bcrypt.hashSync('lohrs2', salt),
-            roles: ['ADA_LH']
-        })
+  // Overtime Reward Gross
+  this.db.collection(collection).insertOne({
+    username: "overtime_reward_gross_2",
+    status: "activated",
+    password: bcrypt.hashSync("otrwgrs2", salt),
+    roles: ["ADA_ORG"],
+  });
 
-    // Overtime Reward Gross
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'overtime_reward_gross_2',
-            password: bcrypt.hashSync('otrwgrs2', salt),
-            roles: ['ADA_ORG']
-        })
+  // No Show Detailed
+  this.db.collection(collection).insertOne({
+    username: "no_show_detailed_2",
+    status: "activated",
+    password: bcrypt.hashSync("nshowd2", salt),
+    roles: ["ADA_NSHD"],
+  });
 
-    // No Show Detailed
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'no_show_detailed_2',
-            password: bcrypt.hashSync('nshowd2', salt),
-            roles: ['ADA_NSHD']
-        })
+  // No Shows
+  this.db.collection(collection).insertOne({
+    username: "no_shows_2",
+    status: "activated",
+    password: bcrypt.hashSync("nshows2", salt),
+    roles: ["ADA_NSH"],
+  });
 
-    // No Shows
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'no_shows_2',
-            password: bcrypt.hashSync('nshows2', salt),
-            roles: ['ADA_NSH']
-        })
+  // Incentives Gross
+  this.db.collection(collection).insertOne({
+    username: "incentives_gross_2",
+    status: "activated",
+    password: bcrypt.hashSync("incntgrs2", salt),
+    roles: ["ADA_IG"],
+  });
 
-    // Incentives Gross
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'incentives_gross_2',
-            password: bcrypt.hashSync('incntgrs2', salt),
-            roles: ['ADA_IG']
-        })
+  // Incentives Net
+  this.db.collection(collection).insertOne({
+    username: "incentives_net_2",
+    status: "activated",
+    password: bcrypt.hashSync("incntnet2", salt),
+    roles: ["ADA_IN"],
+  });
 
-    // Incentives Net
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'incentives_net_2',
-            password: bcrypt.hashSync('incntnet2', salt),
-            roles: ['ADA_IN']
-        })
+  // Mobile Bill
+  this.db.collection(collection).insertOne({
+    username: "mobile_bill_2",
+    status: "activated",
+    password: bcrypt.hashSync("mblbl2", salt),
+    roles: ["ADA_MB"],
+  });
 
-    // Mobile Bill
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'mobile_bill_2',
-            password: bcrypt.hashSync('mblbl2', salt),
-            roles: ['ADA_MB']
-        })
+  // Sick Leave Detailed
+  this.db.collection(collection).insertOne({
+    username: "sick_leave_detailed_2",
+    status: "activated",
+    password: bcrypt.hashSync("sklvde2", salt),
+    roles: ["ADA_SLD"],
+  });
 
-    // Sick Leave Detailed
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'sick_leave_detailed_2',
-            password: bcrypt.hashSync('sklvde2', salt),
-            roles: ['ADA_SLD']
-        })
+  // Sick Leave Cons
+  this.db.collection(collection).insertOne({
+    username: "sick_leave_cons_2",
+    status: "activated",
+    password: bcrypt.hashSync("sklvco2", salt),
+    roles: ["ADA_SLC"],
+  });
 
-    // Sick Leave Cons
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'sick_leave_cons_2',
-            password: bcrypt.hashSync('sklvco2', salt),
-            roles: ['ADA_SLC']
-        })
+  // UKDE Referral
+  this.db.collection(collection).insertOne({
+    username: "ukde_referral_2",
+    status: "activated",
+    password: bcrypt.hashSync("ukde2", salt),
+    roles: ["ADA_UKDE"],
+  });
 
-    // UKDE Referral
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'ukde_referral_2',
-            password: bcrypt.hashSync('ukde2', salt),
-            roles: ['ADA_UKDE']
-        })
+  // Award Gross Amount
+  this.db.collection(collection).insertOne({
+    username: "award_gross_2",
+    status: "activated",
+    password: bcrypt.hashSync("awgrs2", salt),
+    roles: ["ADA_AGA"],
+  });
 
-    // Award Gross Amount
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'award_gross_2',
-            password: bcrypt.hashSync('awgrs2', salt),
-            roles: ['ADA_AGA']
-        })
+  // Gym
+  this.db.collection(collection).insertOne({
+    username: "gym_2",
+    status: "activated",
+    password: bcrypt.hashSync("gym_2", salt),
+    roles: ["ADA_GYM"],
+  });
 
-    // Gym
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'gym_2',
-            password: bcrypt.hashSync('gym_2', salt),
-            roles: ['ADA_GYM']
-        })
+  // Penalties Detailed
+  this.db.collection(collection).insertOne({
+    username: "penalties_detailed_2",
+    status: "activated",
+    password: bcrypt.hashSync("pendet2", salt),
+    roles: ["ADA_PD"],
+  });
 
-    // Penalties Detailed
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'penalties_detailed_2',
-            password: bcrypt.hashSync('pendet2', salt),
-            roles: ['ADA_PD']
-        })
+  // On Call
+  this.db.collection(collection).insertOne({
+    username: "on_call_2",
+    status: "activated",
+    password: bcrypt.hashSync("call2", salt),
+    roles: ["ADA_OC"],
+  });
 
-    // On Call
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'on_call_2',
-            password: bcrypt.hashSync('call2', salt),
-            roles: ['ADA_OC']
-        })
+  // Profile Change
+  this.db
+    .collection(collection)
+    .insertOne({
+      username: "profile_change_2",
+      status: "activated",
+      password: bcrypt.hashSync("profcha2", salt),
+      roles: ["ADA_PC"],
+    })
+    .then(() => done());
 
-    // Profile Change
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'profile_change_2',
-            password: bcrypt.hashSync('profcha2', salt),
-            roles: ['ADA_PC']
-        }).then(() => done());
-
-    // New Hires
-    this.db
-        .collection(collection)
-        .insertOne({
-            username: 'new_hires_2',
-            password: bcrypt.hashSync('nuhire2', salt),
-            roles: ['ADA_NH']
-        }).then(() => done())
-
+  // New Hires
+  this.db
+    .collection(collection)
+    .insertOne({
+      username: "new_hires_2",
+      status: "activated",
+      password: bcrypt.hashSync("nuhire2", salt),
+      roles: ["ADA_NH"],
+    })
+    .then(() => done());
 };
 
 module.exports.down = function (done) {
-    this.db
-        .collection(collection)
-        .drop()
-        .then(() => done());
+  this.db
+    .collection(collection)
+    .drop()
+    .then(() => done());
 };
