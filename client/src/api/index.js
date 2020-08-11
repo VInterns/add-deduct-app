4027815144d6936ba62ab05938bf51c2a91a21f1
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-08-11 19:47:23 +0200

    Export Update User Status API Endpoint

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,12 +2,14 @@ const API_ROOT_PATH = "/api";
 const DATA_API_PATH = "/data";
 const USERS_API_PATH = "/users";
 
+const LIST_USERS_API = `${API_ROOT_PATH}${USERS_API_PATH}/listUsers`;
 const SUBMIT_DATA_API = `${API_ROOT_PATH}${DATA_API_PATH}/submit_data`;
 const EXPORT_DATA_API = `${API_ROOT_PATH}${DATA_API_PATH}/export_data`;
-const LIST_USERS_API = `${API_ROOT_PATH}${USERS_API_PATH}/listUsers`;
+const UPDATE_USER_STATUS = `${API_ROOT_PATH}${USERS_API_PATH}/status}`;
 
 module.exports = {
   SUBMIT_DATA_API,
   EXPORT_DATA_API,
   LIST_USERS_API,
+  UPDATE_USER_STATUS,
 };
