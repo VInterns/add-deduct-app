a6c8c1bec1e54a87bebf4fb40a07fb1db285ca02
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-08-12 19:20:12 +0200

    Add setDeadline API

--- server/routes/info.js
+++ server/routes/info.js
@@ -1,29 +1,48 @@
 const { Router } = require("express");
 
 module.exports = (db) => {
-    const collection = "codes"
-    const router = new Router();
-    router.get("/", (req, res) => {
-        // Any Body can use the db here
-        // ex: db.collection('COLLECTION_NAME');
-        res.json({ environment: process.env.NODE_ENV });
+  const collection = "codes";
+  const router = new Router();
+  router.get("/", (req, res) => {
+    res.json({ environment: process.env.NODE_ENV });
+  });
+
+  router.post("/verifyCode", (req, res) => {
+    db.collection(collection).findOne({ email: req.body.email }, function (
+      err,
+      record
+    ) {
+      if (err) throw err;
+      if (record.code === req.body.toVerify) {
+        return res.status(200).json({
+          response: "Code Verified",
+        });
+      } else {
+        return res.status(400).json({
+          response: "Code not Verified",
+        });
+      }
     });
+  });
 
-    router.post("/verifyCode", (req, res) => {
-        //console.log("/info/verifyCode", req.body)
-        db.collection(collection).findOne({ email: req.body.email }, function (err, record) {
-            if (err) throw err;
-            if (record.code === req.body.toVerify) {
-                return res.status(200).json({
-                    "response": "Code Verified"
-                })
-            } else {
-                return res.status(400).json({
-                    "response": "Code not Verified"
-                })
-            }
-        })
-    })
-    return router;
+  // setDeadline
+  router.post("/setDeadline", (req, res) => {
+    const { endDate } = req.body;
+    const startDate = new Date();
+    // startDate = `${startDate.getFullYear()}-${startDate.getMonth()}-${startDate.getDay()}`;
+    db.collection("deadlines").insertOne(
+      {
+        createdAt: startDate,
+        deadline: endDate,
+      },
+      (err, doc) => {
+        if (err) {
+          res.json({ message: err }).end();
+        }
+
+        res.status(200).json({ message: "Successfully set deadline" }).end();
+      }
+    );
+  });
+  return router;
 };
-
