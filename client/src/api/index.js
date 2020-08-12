083a97c9e862758e42eb1cb6e19f8770c27a09cf
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-08-12 19:20:35 +0200

    Export setDeadline API URL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,14 @@
 const API_ROOT_PATH = "/api";
 const DATA_API_PATH = "/data";
 const USERS_API_PATH = "/users";
+const INFO_API_PATH = "/info";
 
 const DELETE_USER = `${API_ROOT_PATH}${USERS_API_PATH}/deleteUser`;
 const LIST_USERS_API = `${API_ROOT_PATH}${USERS_API_PATH}/listUsers`;
 const SUBMIT_DATA_API = `${API_ROOT_PATH}${DATA_API_PATH}/submit_data`;
 const EXPORT_DATA_API = `${API_ROOT_PATH}${DATA_API_PATH}/export_data`;
 const UPDATE_USER_STATUS = `${API_ROOT_PATH}${USERS_API_PATH}/changeStatus`;
+const DEADLINE_SUBMIT_API = `${API_ROOT_PATH}${INFO_API_PATH}/setDeadline`;
 
 module.exports = {
   SUBMIT_DATA_API,
@@ -14,4 +16,5 @@ module.exports = {
   LIST_USERS_API,
   UPDATE_USER_STATUS,
   DELETE_USER,
+  DEADLINE_SUBMIT_API,
 };
