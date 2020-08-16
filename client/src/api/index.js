85dfad7f6ae2533d30e17bd8399dcd451031da04
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-08-16 20:44:31 +0200

    Export Deadline API URLs & Categorize APIs

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,12 +3,19 @@ const DATA_API_PATH = "/data";
 const USERS_API_PATH = "/users";
 const INFO_API_PATH = "/info";
 
+// USERS
 const DELETE_USER = `${API_ROOT_PATH}${USERS_API_PATH}/deleteUser`;
 const LIST_USERS_API = `${API_ROOT_PATH}${USERS_API_PATH}/listUsers`;
+const UPDATE_USER_STATUS = `${API_ROOT_PATH}${USERS_API_PATH}/changeStatus`;
+
+// DATA
 const SUBMIT_DATA_API = `${API_ROOT_PATH}${DATA_API_PATH}/submit_data`;
 const EXPORT_DATA_API = `${API_ROOT_PATH}${DATA_API_PATH}/export_data`;
-const UPDATE_USER_STATUS = `${API_ROOT_PATH}${USERS_API_PATH}/changeStatus`;
-const DEADLINE_SUBMIT_API = `${API_ROOT_PATH}${INFO_API_PATH}/setDeadline`;
+
+// DEADLINES
+const SET_DEADLINE_API = `${API_ROOT_PATH}${INFO_API_PATH}/setDeadline`;
+const GET_DEADLINE_API = `${API_ROOT_PATH}${INFO_API_PATH}/getDeadline`;
+const DEL_DEADLINE_API = `${API_ROOT_PATH}${INFO_API_PATH}/delDeadline`;
 
 module.exports = {
   SUBMIT_DATA_API,
@@ -16,5 +23,7 @@ module.exports = {
   LIST_USERS_API,
   UPDATE_USER_STATUS,
   DELETE_USER,
-  DEADLINE_SUBMIT_API,
+  SET_DEADLINE_API,
+  GET_DEADLINE_API,
+  DEL_DEADLINE_API,
 };
