1aaaaeaf7e9871cce133ce233695c82a70a732a5
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-08-16 21:26:44 +0200

    Update message to info & display deadline date

--- client/src/components/TeamBodyNotice.js
+++ client/src/components/TeamBodyNotice.js
@@ -1,38 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Message, Icon } from "semantic-ui-react";
+import { Message } from "semantic-ui-react";
 
-export const TeamBodyNotice = (props) => {
-    let { teamName, fileName } = props;
-    return (
-        <div className='container-fluid mt-4'>
-            <Message warning>
-                <div className='d-flex flex-row align-items-center'>
-                    <Icon name='info circle' size='large' />
-                    <div>
-                        <span className='h5'>
-                            {
-                                "You have to use the sample excel sheet for your "
-                                + teamName.toLowerCase() + " data entry."
-                                + " Download it "
-                            }
-                        </span>
-                        <a
-                            href={fileName}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            download
-                        >
-                            <span className='font-weight-bolder font-italic h4'>here.</span>
-                        </a>
-                    </div>
-                </div>
-            </Message>
-        </div>
-    );
-}
+export const TeamBodyNotice = ({ teamName, fileName, deadline }) => {
+  return (
+    <div className="container-fluid mt-4">
+      <Message info>
+        <Message.Header>Important Notice</Message.Header>
+        <Message.List>
+          <Message.Item>
+            {"You have to use the sample excel sheet for your " +
+              teamName.toLowerCase() +
+              " data entry." +
+              " Download it "}
+            <a
+              href={fileName}
+              target="_blank"
+              rel="noopener noreferrer"
+              download
+            >
+              <span className="font-weight-bolder font-italic h5">here.</span>
+            </a>
+          </Message.Item>
+          <Message.Item>{`Your have to submit your data by ${deadline} or you won't be able to submit.`}</Message.Item>
+        </Message.List>
+      </Message>
+    </div>
+  );
+};
 
 TeamBodyNotice.protoTypes = {
-    teamName: PropTypes.string.isRequired,
-    fileName: PropTypes.string.isRequired
-}
+  teamName: PropTypes.string.isRequired,
+  fileName: PropTypes.string.isRequired,
+  deadline: PropTypes.string.isRequired,
+};
