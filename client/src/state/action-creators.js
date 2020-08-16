31b0715e293c1eb8bf844984d4eef90bf53cbf06
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-08-16 21:38:30 +0200

    Update indentation & remove console.log

--- client/src/state/action-creators.js
+++ client/src/state/action-creators.js
@@ -1,5 +1,5 @@
-import * as actions from './actions';
-var randomize = require('randomatic');
+import * as actions from "./actions";
+var randomize = require("randomatic");
 
 export const login = (credentials) => (dispatch) =>
   makeRequest(
@@ -8,11 +8,11 @@ export const login = (credentials) => (dispatch) =>
     {
       method: "POST",
       headers: { "content-type": "application/json" },
-      body: JSON.stringify(credentials)
+      body: JSON.stringify(credentials),
     },
-    res => {
+    (res) => {
       if (res.status === 200) {
-        return res.json().then(account => {
+        return res.json().then((account) => {
           dispatch(loginSuccessful(account));
         });
       } else {
@@ -21,161 +21,163 @@ export const login = (credentials) => (dispatch) =>
     }
   );
 
-export const signup = (credentials) => (dispatch) => 
+export const signup = (credentials) => (dispatch) =>
   makeRequest(
     dispatch,
     "api/users/addPassword",
     {
       method: "POST",
-      headers: { "Content-Type": "application/json"},
-      body: JSON.stringify(credentials)
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials),
     },
-    res => {
-      if (res.status === 200){
-        return res.json().then(result => {
+    (res) => {
+      if (res.status === 200) {
+        return res.json().then((result) => {
           dispatch(signupSuccessful());
-        })
-      } else{
-        dispatch(signupFailed())
+        });
+      } else {
+        dispatch(signupFailed());
       }
     }
-  )
+  );
 
 export const sendCode = (email) => (dispatch) => {
-  let otp = randomize('0000000');
+  let otp = randomize("0000000");
 
   makeRequest(
     dispatch,
     "api/mail/sendMail",
     {
       method: "POST",
-      headers: { "Content-Type": "application/json"},
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         mailList: email,
         code: otp,
         subject: "Leaver App Signup Code",
-        text: "Dear user,\n\nPlease find the following code to use it in your signup process to the Leaver App system.\n\nSignup Code: " + otp
-      })
+        text:
+          "Dear user,\n\nPlease find the following code to use it in your signup process to the Leaver App system.\n\nSignup Code: " +
+          otp,
+      }),
     },
-    res => {
-      if (res.status === 200){
-        return res.json().then(result => {
-          dispatch(sendCodeSuccessful())
-        })
+    (res) => {
+      if (res.status === 200) {
+        return res.json().then((result) => {
+          dispatch(sendCodeSuccessful());
+        });
       } else {
-        console.log(res);
-        dispatch(sendCodeFailed())
+        dispatch(sendCodeFailed());
       }
     }
-  )
-}
+  );
+};
 
 export const verifyCode = (user) => (dispatch) => {
-
   makeRequest(
     dispatch,
     "api/info/verifyCode",
     {
       method: "POST",
-      headers: { "Content-Type": "application/json"},
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         email: user.username,
-        toVerify: user.code
-      })
+        toVerify: user.code,
+      }),
     },
-    res => {
-      if (res.status === 200){
-        return res.json().then(result => {
+    (res) => {
+      if (res.status === 200) {
+        return res.json().then((result) => {
           console.log("Code Verified", result);
-          dispatch(verifyCodeSuccess())
-        })
+          dispatch(verifyCodeSuccess());
+        });
       } else {
-        dispatch(verifyCodeFailed())
+        dispatch(verifyCodeFailed());
       }
     }
-  )
-}
+  );
+};
 
 export const sendCodeSuccessful = () => ({
-  type: actions.SEND_CODE_SUCCESS
-})
+  type: actions.SEND_CODE_SUCCESS,
+});
 
-export const initializeApp= ()=>{
-  return { 
-    type:actions.INIT_STATE
+export const initializeApp = () => {
+  return {
+    type: actions.INIT_STATE,
   };
-}
+};
 
 export const verifyCodeSuccess = () => ({
-  type:actions.VERIFY_CODE_SUCCESS
-})
+  type: actions.VERIFY_CODE_SUCCESS,
+});
 
 export const verifyCodeFailed = () => ({
-  type:actions.VERIFY_CODE_FAILED
-})
+  type: actions.VERIFY_CODE_FAILED,
+});
 
 export const sendCodeFailed = () => ({
-  type: actions.SEND_CODE_FAILED
-})
+  type: actions.SEND_CODE_FAILED,
+});
 
 export const signupSuccessful = () => ({
-  type: actions.SIGNUP_SUCCESS
-})
+  type: actions.SIGNUP_SUCCESS,
+});
 
 export const signupFailed = () => ({
-  type: actions.SIGNUP_FAILED
-})
-
+  type: actions.SIGNUP_FAILED,
+});
 
 export const loginSuccessful = (account: Account) => ({
   type: actions.LOGIN_SUCCEESS,
-  payload: account
+  payload: account,
 });
 
 export const loginFailed = () => ({
-  type: actions.LOGIN_FAILED
+  type: actions.LOGIN_FAILED,
 });
 
 export const logoutSuccessful = () => ({
-  type: actions.LOGOUT
+  type: actions.LOGOUT,
 });
 
 export const logout = () => (dispatch) => {
-  makeRequest(dispatch, "/api/login", {
-    method: "DELETE",
-    headers: { "content-type": "application/json" }
-  }, res => {
-    if (res.status === 204) {
-      dispatch(logoutSuccessful());
+  makeRequest(
+    dispatch,
+    "/api/login",
+    {
+      method: "DELETE",
+      headers: { "content-type": "application/json" },
+    },
+    (res) => {
+      if (res.status === 204) {
+        dispatch(logoutSuccessful());
+      }
     }
-  });
+  );
 };
 
 export const requestStarted = () => ({
-  type: actions.SET_LOADING
+  type: actions.SET_LOADING,
 });
 
 export const requestFinished = () => ({
-  type: actions.SET_NOT_LOADING
+  type: actions.SET_NOT_LOADING,
 });
 
 export const makeRequest = (
   dispatch,
   url,
   options = undefined,
-  callback = () => { }
+  callback = () => {}
 ) => {
   dispatch(requestStarted());
   return fetch(url, options)
-    .then(res => callback(res))
-    .then(value => {
+    .then((res) => callback(res))
+    .then((value) => {
       dispatch(requestFinished());
       return value;
     })
-    .catch(err => {
+    .catch((err) => {
       dispatch(requestFinished());
       throw err;
     });
 };
-
-
